b458684773d1246237dabda04acdbada60a99b04
siddharthrainit <siddharthrainit@gmail.com>
2024-12-28 11:45:18 +0530

    "Test Commit"

--- PlatformService/PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c35952f6bc0635ad0aaa0061d76b3eb7fb3ab84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c5488ac51ad9855c7c34e5c3ef0cf2a7875cc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
